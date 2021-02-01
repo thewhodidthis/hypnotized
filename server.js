c9ece4922efc5ccb5e1fb21b36bc752939018bc1
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-02-01 23:51:17 +0000

    server: allow for cert loading via config.json

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 'use strict'
 
-const { Server: WebSocketServer } = require('ws')
+const { existsSync, readFileSync } = require('fs')
+const https = require('https')
+const { Server } = require('ws')
 
-const io = new WebSocketServer({
-  port: process.env.PORT || process.env.npm_package_config_port || 8014,
-  path: '/io'
-})
+const { cert, key } = require('./config.json')
+
+const port = process.env.PORT || process.env.npm_package_config_port || 8014
+const options = { path: '/io' }
+
+if (existsSync(cert) && existsSync(key)) {
+  options.server = https
+    .createServer({ cert: readFileSync(cert), key: readFileSync(key) })
+    .listen(port)
+} else {
+  options.port = port
+}
+
+const io = new Server(options)
 
 const bumps = []
 const maxEntries = process.env.MAX || 1000
